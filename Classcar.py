74600d80a71cc2ec0fc20d1a533375c7b7c77140
Souhaïl Bechchari <146872818+SouhailBechchari@users.noreply.github.com>
2023-11-19 22:50:50 +0100

    Create Classcar.py

---
+++ Classcar.py
@@ -0,0 +1,20 @@
+class Car:
+    
+    def __init__(self,speed,color,nitrospeed,model):
+        self.speed = speed
+        self.color = color
+        self.nitrospeed = nitrospeed
+        self.model = model
+
+    def turn():
+        pass
+        
+    def accelerate():
+        pass
+    
+    
+    def brake():
+        pass
+    
+    def boost():
+        pass
